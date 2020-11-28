97d1e626dc500a9470da1452dfd8ddedbf477016
Pazit <pazitrose1999@gmail.com>
2020-11-28 10:32:32 -0500

    Added navbar and routes to necessary pages

--- server.py
+++ server.py
@@ -10,5 +10,21 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/getting-started')
+def templates():
+   return render_template('getting-started.html')
+
+@app.route('/templates')
+def templates():
+   return render_template('templates.html')
+
+@app.route('/guides')
+def guides():
+   return render_template('guides.html')
+
+@app.route('/forum')
+def forum():
+   return render_template('forum.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
--- static/index.js
+++ static/index.js
@@ -1,1 +1,15 @@
 // filler file
+
+$(document).ready(function () {
+  $("#templates-link").click(function () {
+    window.location.href = "http://127.0.0.1:5000/templates";
+  });
+
+  $("#guides-link").click(function () {
+    window.location.href = "http://127.0.0.1:5000/guides";
+  });
+
+  $("#forum-link").click(function () {
+    window.location.href = "http://127.0.0.1:5000/forum";
+  });
+});
